b6c1bc3fe14aeccc8fac4514598850138a317e57
Logan Shire <logan.shire@gmail.com>
2017-04-03 01:04:07 -0700

    Making fields non-optional

--- Sources/ObjectSchema.swift
+++ Sources/ObjectSchema.swift
@@ -2,8 +2,8 @@ import ObjectMapper
 
 public struct ObjectSchema {
     public let metadata: Metadata
-    public let required: [String]?
-    public let properties: [String : Schema]?
+    public let required: [String]
+    public let properties: [String : Schema]
     public let minProperties: Int?
     public let maxProperties: Int?
     public let additionalProperties: Either<Bool, Schema>
@@ -14,27 +14,25 @@ struct ObjectSchemaBuilder: Builder {
     typealias Building = ObjectSchema
 
     let metadata: MetadataBuilder
-    let required: [String]?
-    let properties: [String : SchemaBuilder]?
+    let required: [String]
+    let properties: [String : SchemaBuilder]
     let minProperties: Int?
     let maxProperties: Int?
     let additionalProperties: Either<Bool, SchemaBuilder>
 
     init(map: Map) throws {
         metadata = try MetadataBuilder(map: map)
-        required = try? map.value("required")
-        properties = try? map.value("properties")
+        required = (try? map.value("required")) ?? []
+        properties = (try? map.value("properties")) ?? [:]
         minProperties = try? map.value("minProperties")
         maxProperties = try? map.value("maxProperties")
         additionalProperties = (try? Either(map: map, key: "additionalProperties")) ?? .a(false)
     }
 
     func build(_ swagger: SwaggerBuilder) throws -> ObjectSchema {
-        let properties = try self.properties.map { dictionary in
-            return try Dictionary(dictionary.map { (key, value) in
-                return (key, try value.build(swagger))
-            })
-        }
+        let properties = try Dictionary(self.properties.map { (key, value) in
+            return (key, try value.build(swagger))
+        })
 
         let additionalProperties: Either<Bool, Schema>
         switch self.additionalProperties {
