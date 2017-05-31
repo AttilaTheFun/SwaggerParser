59d1d138261697b56e0f1ca629e86d1d7cdb8781
Westin Newell <n8chur@users.noreply.github.com>
2017-05-31 11:13:48 -0700

    `allOf` support (#6)
    * `allOf`, `x-nullable`, `x-abstract`, `example`/`x-example`, separated spec files, and fixes
    
    Adds support for:
    - `allOf`
    - `x-nullable`
    - `discriminator`
    - `x-abstract`
    - `example`/`x-example`
    - Separated Swagger spec files (e.g. Base.json, Models/ModelA.json, Models/ModelB.json, etc.)
    - Moved test fixture to a file
    - Added more unit tests
    
    * Revert "`allOf`, `x-nullable`, `x-abstract`, `example`/`x-example`, separated spec files, and fixes"
    
    This reverts commit 4d90afcdf3ef46ad0872f7909837b12f5f874ed6.
    
    Splitting this up into smaller PRs.
    
    * Add `allOf` support
    
    Adds support for `allOf`.
    
    Additionally moves test fiture to a separate file, rather than being defined in-line.
    
    * Updates based on PR feedback.
    
    * Remove extra new line.
    
    * More updates based no PR feedback.

---
+++ Sources/AllOfSchema.swift
@@ -0,0 +1,25 @@
+import ObjectMapper
+
+public struct AllOfSchema {
+    public let metadata: Metadata
+    public let subschemas: [Schema]
+}
+
+public struct AllOfSchemaBuilder {
+    
+    typealias Building = AllOfSchema
+    
+    let metadata: MetadataBuilder
+    let schemaBuilders: [SchemaBuilder]
+    
+    init(map: Map) throws {
+        metadata = try MetadataBuilder(map: map)
+        
+        schemaBuilders = try map.value("allOf")
+    }
+    
+    func build(_ swagger: SwaggerBuilder) throws -> AllOfSchema {
+        let subschemas = try schemaBuilders.map { try $0.build(swagger) }
+        return AllOfSchema(metadata: try self.metadata.build(swagger), subschemas: subschemas)
+    }
+}
