bad0ddd7a10198e6948801461ebb55ae323f1317
Yonas Kolb <yonaskolb@gmail.com>
2017-07-07 19:05:32 +0200

    supports versions that are parsed as numbers as opposed to strings

--- Sources/Version.swift
+++ Sources/Version.swift
@@ -4,9 +4,10 @@ public class VersionTransform: TransformType {
     public typealias Object = Version
     public typealias JSON = String
     public func transformFromJSON(_ value: Any?) -> Version? {
-        guard let string = value as? String else {
+        guard let string = (value as? LosslessStringConvertible)?.description else {
             return nil
         }
+
         let components = string.components(separatedBy: ".")
         guard components.count >= 1, let major = Int(components[0]) else {
             return nil
