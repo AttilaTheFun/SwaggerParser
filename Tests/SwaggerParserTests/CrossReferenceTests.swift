f49c47a6127ee901902f21c200deb39db2922a79
Yonas Kolb <yonaskolb@gmail.com>
2017-07-08 14:04:15 +0200

    make cross reference test stricter and make it pass

--- Tests/SwaggerParserTests/CrossReferenceTests.swift
+++ Tests/SwaggerParserTests/CrossReferenceTests.swift
@@ -8,9 +8,25 @@ class CrossReferenceTests: XCTestCase {
 
         guard
             let definition = swagger.definitions.first(where: { $0.name == "Foo" }),
-            case .structure = definition.structure else
+            case .object = definition.structure else
         {
-            return XCTFail("Foo is not a structure schema.")
+            return XCTFail("Foo is not an object schema.")
+        }
+        guard
+            case let .object(objectSchema) = definition.structure,
+            let property = objectSchema.properties.values.first,
+            case let .structure(_, structure) = property,
+            structure.name == "Bar" else
+        {
+            return XCTFail("Foo does not contain reference to Bar.")
+        }
+        guard
+            case let .object(objectSchema2) = structure.structure,
+            let property2 = objectSchema2.properties.values.first,
+            case let .structure(_, structure2) = property2,
+            structure2.name == "Foo" else
+        {
+            return XCTFail("Bar does not contain reference to Foo.")
         }
     }
 }
